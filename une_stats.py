42ab6d0d8517e037074fb6f15702699330594f1a
Manuel <manuelcastr88@gmail.com>
2023-04-20 11:11:31 -0300

    real outage data added to the plot

--- une_stats.py
+++ une_stats.py
@@ -11,7 +11,7 @@ def daterange(start_date, end_date):
 
 
 def plot_daily_generation_and_demand(headers, dates, data, suptitle='', **kwargs):
-    generation, demand, outage = data.T
+    generation, demand, outage_estimate, real_outage = data.T
     date_step = kwargs.get('date_step', 1)
     n_days = len(dates)
 
@@ -20,13 +20,18 @@ def plot_daily_generation_and_demand(headers, dates, data, suptitle='', **kwargs
     plt.suptitle(suptitle)
 
     x = np.arange(n_days)
-    axes.fill_between(x, np.zeros(n_days), generation, color='tab:green', edgecolor='k', lw=.8, zorder=100, label=headers[0])
-    axes.plot(x, demand, color='tab:red', lw='3', zorder=200, label=headers[1])
-    axes.fill_between(x, np.zeros(n_days), outage, color='tab:brown', edgecolor='k', lw=1.2, zorder=300, label=headers[2])
+    axes.fill_between(x, np.zeros(n_days), generation,
+                      color='tab:green', edgecolor='k', lw=.8, zorder=100, label=headers[0])
+    axes.plot(x, demand,
+              color='tab:red', lw='3', zorder=200, label=headers[1])
+    axes.fill_between(x, np.zeros(n_days), outage_estimate,
+                      color='tab:brown', edgecolor='k', lw=1.2, zorder=300, label=headers[2])
+    axes.plot(x, real_outage,
+              color='firebrick', lw='2', ls=(0, (5, 1)), zorder=400, label=headers[3])
 
     x_labels = np.array([''] * len(dates), dtype=object)
     x_labels[::date_step] = np.array([d.strftime("%d/%m") for d in dates], dtype=object)[::date_step]
-    axes.set_xticks(range(n_days), x_labels, rotation=-45, fontsize='x-small')
+    axes.set_xticks(range(n_days), x_labels, rotation=-70, fontsize='x-small')
 
     axes.set_xlabel('Fecha')
     axes.set_ylabel('MWh', rotation=0)
@@ -62,17 +67,17 @@ def report_une_stats(data_filename, output_path, show=False):
     output_path = pathlib.Path(output_path)
 
     fig_generation = plot_daily_generation_and_demand(
-        headers[[1,2,4]],
+        headers[[1,2,4,5]],
         dates,
-        une_data[:, [0, 1, 3]],
+        une_data[:, [0, 1, 3, 4]],
         suptitle='Estimado de Generación vs Demanda en horario pico',
-        date_step=2, show=show)
+        date_step=3, show=show)
 
     fig_generation.savefig(str((output_path / 'fig_01_generation_vs_demand.png').absolute()))
 
 
 if __name__ == '__main__':
-    report_une_stats('une_cuba_diario.txt',
+    report_une_stats('une_cuba_diario_plus.txt',
                      'c:/Users/User/',
                      show=True)
 
